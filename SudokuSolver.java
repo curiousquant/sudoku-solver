7ec05600453e89ff8e297cd00149620e9c3defb6
jguy <jon.mun91@gmail.com>
2024-08-06 16:15:49 -0400

    bug fix2

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -12,13 +12,13 @@ public class SudokuSolver{
         System.out.println("Hello World");
         Integer[] ex1_bAlmostSolved = {4,9,7,8,6,1,3,5,2,
         8,5,1,2,4,3,7,9,6,
-        6,0,0,0,5,7,1,8,4,
-        7,0,0,6,2,5,8,1,9,
+        6,3,2,9,5,7,1,8,4,
+        7,4,3,6,2,5,8,1,9,
         2,1,5,3,8,9,4,6,7,
         9,8,6,7,1,4,2,3,5,
-        5,7,9,4,3,8,6,2,0,
+        5,7,9,4,3,8,6,2,1,
         1,6,8,5,7,2,9,4,3,
-        3,0,0,1,9,0,5,7,0};
+        3,2,4,1,9,6,5,7,8};
         Integer[] ex1_bOriginal = {4,0,0,0,6,0,0,5,2,
           0,5,0,0,4,0,7,0,0,
           6,3,2,0,0,0,1,8,0,
@@ -58,7 +58,7 @@ public class SudokuSolver{
       for(int i=0;i<this.getBoard().length;i++){
         initboard.add(b.get(i));
       }
-      while(b.contains(0)){
+      while(!this.checkBoard(b)){
         
         //System.out.println(board);
         for(int i=0;i<ROW_SIZE;i++){
@@ -207,7 +207,7 @@ public class SudokuSolver{
             }
             
             if(count>1){
-              return false && containZero;
+              return false;
             }
           }
           for(int k=1;k<10;k++){
@@ -219,13 +219,13 @@ public class SudokuSolver{
               //System.out.println(count+"!"+k+"!"+cols.get(l)+"!"+rows+"!"+cols);
             }
             if(count>1){
-              return false && containZero;
+              return false;
             }
           }
         }
       }
       
-      return true && containZero;
+      return containZero;
     }
 
     public List<Integer> solve2(){
